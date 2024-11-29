cf6453991c991e4cd3e06329a7b92aaa45a3539f
justinjewell <jjewe60@wgu.edu>
2024-11-29 11:09:50 -0600

    dependcy updates

--- src/database/sequelize.js
+++ src/database/sequelize.js
@@ -1,10 +1,25 @@
 // src/database/sequelize.js
 import { Sequelize } from 'sequelize'
 import { join } from 'path'
+import { app } from 'electron'
+import { is } from '@electron-toolkit/utils'
+
+let storagePath
+
+if (is.dev) {
+  // Use a local database file in development mode
+  storagePath = join(__dirname, 'database.sqlite')
+} else {
+  // Use the user data directory for the database file in production mode
+  const userDataPath = app.getPath('userData')
+  storagePath = join(userDataPath, 'database.sqlite')
+}
+
+console.log('Using database file:', storagePath)
 
 const sequelize = new Sequelize({
   dialect: 'sqlite',
-  storage: join(__dirname, 'database.sqlite')
+  storage: storagePath
 })
 
 export { sequelize }
